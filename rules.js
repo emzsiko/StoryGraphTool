8f01a22d915aeca0297705a011cf05268d11e2f3
em <eishida17@gmail.com>
2024-04-16 15:44:41 -0700

    first draft

--- rules.js
+++ rules.js
@@ -76,28 +76,6 @@ class KnifeLocation extends Scene {
     }
 }
 
-/*class Fight extends Location {
-    create(key) {
-        super.create(key);
-        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
-
-        if(locationData.Locked && locationData.Locked.length > 0) { // TODO: check if the location has any Choices
-            for(let lockoption of locationData.Locked) { // TODO: loop over the location's Choices
-                this.engine.addChoice(lockoption.Text, lockoption.Target); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
-            }
-        }
-    }
-    handleChoice(choice) {
-        if(choice) {
-            this.engine.show("> "+ choice);
-            this.engine.gotoScene(Interaction, choice);
-        } else {
-            this.engine.gotoScene(End);
-        }
-    }
-} */
-
 class End extends Scene {
     create() {
         this.engine.show("<hr>");
